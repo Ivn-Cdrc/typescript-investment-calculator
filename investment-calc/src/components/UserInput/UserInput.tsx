79ce1119b082002b5b79232e6ace51de20ecc3d5
Ivn-Cdrc <ivan.cedric.bulosan@gmail.com>
2023-10-03 04:57:13 +1100

    implemented state, change handlers and 2-way binding in UserInput.tsx

--- investment-calc/src/components/UserInput/UserInput.tsx
+++ investment-calc/src/components/UserInput/UserInput.tsx
@@ -1,39 +1,102 @@
+import { ChangeEvent, FormEvent, FormEventHandler, MouseEvent, useState } from "react";
+
+interface UserInputData {
+	currentSavings: number;
+	yearlyContribution: number;
+	expectedReturn: number;
+	duration: number;
+}
+
+const initialUserInput: UserInputData = {
+	currentSavings: 10000,
+	yearlyContribution: 1200,
+	expectedReturn: 7,
+	duration: 10
+}
 
 function UserInput() {
-	return (
-		<form className="form">
-			<div className="input-group">
-				<p>
-					<label htmlFor="current-savings">Current Savings ($)</label>
-					<input type="number" id="current-savings" />
-				</p>
-				<p>
-					<label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-					<input type="number" id="yearly-contribution" />
-				</p>
-			</div>
-			<div className="input-group">
-				<p>
-					<label htmlFor="expected-return">
-						Expected Interest (%, per year)
-					</label>
-					<input type="number" id="expected-return" />
-				</p>
-				<p>
-					<label htmlFor="duration">Investment Duration (years)</label>
-					<input type="number" id="duration" />
-				</p>
-			</div>
-			<p className="actions">
-				<button type="reset" className="buttonAlt">
-					Reset
-				</button>
-				<button type="submit" className="button">
-					Calculate
-				</button>
-			</p>
-		</form>
-	);
+	const [userInput, setUserInput] = useState<UserInputData>(initialUserInput);
+
+  function submitHandler(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+		console.log(userInput);
+  }
+
+  function resetHandler(event: MouseEvent<HTMLElement>) {
+    setUserInput(initialUserInput);
+  }
+
+  function inputChangeHandler(input: string, value: string) {
+		setUserInput((prevInput: UserInputData) => {
+			return {
+				...prevInput, 
+				[input]: value
+			};
+		});
+	}
+
+  return (
+    <form onSubmit={submitHandler} className="form">
+      <div className="input-group">
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("currentSavings", event.target.value)
+            }
+						value={userInput.currentSavings}
+            type="number"
+            id="current-savings"
+          />
+        </p>
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("yearlyContribution", event.target.value)
+            }
+						value={userInput.yearlyContribution}
+            type="number"
+            id="yearly-contribution"
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("expectedReturn", event.target.value)
+            }
+						value={userInput.expectedReturn}
+            type="number"
+            id="expected-return"
+          />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input
+            onChange={(event) =>
+              inputChangeHandler("duration", event.target.value)
+            }
+						value={userInput.duration}
+            type="number"
+            id="duration"
+          />
+        </p>
+      </div>
+      <p className="actions">
+        <button onClick={resetHandler} type="reset" className="buttonAlt">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Calculate
+        </button>
+      </p>
+    </form>
+  );
 }
 
 export default UserInput;
